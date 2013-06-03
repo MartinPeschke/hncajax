8b94f1c4dbf5965738648be57f49b95f96dbcc87
Martin Peschke <martin@per-4.com>
2013-06-03 16:21:35 +0200

    youtube updated, removed switcher, only loader now

--- tools/youtube.js
+++ tools/youtube.js
@@ -1,30 +1,25 @@
 define(["tools/hash", "tools/ajax"], function(hashlib, ajax){
-    var setUpPlayer = function(root){
-        var currentVideo;
+    var setUpPlayer = function(params){
+
+        var currentVideo
+            , root = params.root||$(document)
+            , aspect = params.aspect || 0.5625
+            , quality = params.quality || "hd720";
+
         root.find('.you-tube-container').each(function(idx, elem){
-            var elemId = 'youtube-'+hashlib.UUID()
-                , initial = root.find(".active").find("a").data("videoId");
+            var elemId = 'youtube-'+hashlib.UUID(), initial = $(elem).data("videoId");
             $(elem).append('<div id="'+elemId+'"></div>');
             currentVideo = new YT.Player(elemId, {
-                height: $(elem).width() * 0.5625,
+                height: $(elem).width() * aspect,
                 width: $(elem).width(),
                 videoId: initial,
-                suggestedQuality: "hd720",
-                playerVars : {controls:1, autohide:1, modestbranding :1, theme :'dark', wmode: "opaque"},
+                suggestedQuality: quality,
+                playerVars : {controls:0, autohide:1, modestbranding :1, theme :'dark', wmode: "opaque"},
                 events: {
                     onReady: function(event){event.target.setPlaybackQuality("hd720")}
                 }
             });
         });
-        root.find('[data-toggle="video"]').on({click:function(e){
-            var $elem = $(e.target)
-                , resumeTime = parseFloat($(e.target).data("resumeTime") || 0);
-            $elem.parent().addClass("active").siblings(".active").removeClass("active").find('[data-toggle="video"]').data({resumeTime: currentVideo.getCurrentTime()});
-            currentVideo.cueVideoById($elem.data("videoId"), resumeTime, "hd720");
-            e.preventDefault();
-            e.stopPropagation();
-            return false;
-        }});
     }, loaded = $.Deferred();
     window.onYouTubePlayerAPIReady = function () {
         loaded.resolve();
@@ -34,8 +29,8 @@ define(["tools/hash", "tools/ajax"], function(hashlib, ajax){
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
     return {
-        onLoad: function(root){
-            loaded.done(_.bind(setUpPlayer, this, root));
+        onLoad: function(params){
+            loaded.done(_.bind(setUpPlayer, this, params));
         }
     }
 });
